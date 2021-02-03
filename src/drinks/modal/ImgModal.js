edfbfb4446e9d9307b759775812c571ca552e1bd
Radek W <kodzi12342@gmail.com>
2021-02-03 14:45:42 +0100

    GH Deploy commit

---
+++ src/drinks/modal/ImgModal.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './ImgModal.css';
+
+const ImgModal = ({show, onClose,img}) => {
+    if(!show){
+        return null;
+    }
+
+    return (
+        <div onClick={onClose} className='modalFallback'>
+            <img className='modalMain' src={img} />
+        </div>
+    )
+}
+
+export default ImgModal;
+
+
+// <div style={{backgroundImage: `url(${img})`}} className='modalMain'>
+// </div>
